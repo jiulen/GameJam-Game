df987338bbcf8fc143c805ec87027a3ba6d6641a
jiulen <wongjiulen.coding@gmail.com>
2023-07-31 16:48:48 +0800

    minimap progress
    missing part to show room progress

---
+++ Assets/GameJam/Minimap/MinimapManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapManager : MonoBehaviour
+{
+    public MinimapRoom[] rooms;
+    public Image[] connectorsH;
+    public Image[] connectorsV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0 ; i < rooms.Length; ++i)
+        {
+            if (i / 7 < 6) rooms[i].connectorBottom = connectorsH[i];
+            if (i % 7 > 0) rooms[i].connectorBottom = connectorsV[i - i / 7 - 1];
+            if (i % 7 < 6) rooms[i].connectorBottom = connectorsV[i - i / 7];
+            if (i / 7 > 0) rooms[i].connectorTop = connectorsH[i - 7];
+
+            rooms[i].SetRoom(false);
+        }
+    }
+}
