fd701b817441a016b115c5e3fa6c1aaeb98654d8
jiulen <wongjiulen.coding@gmail.com>
2023-08-04 17:15:15 +0800

    boss tp room progress

--- Assets/Scripts/Rogue/TeleportPlayer.cs
+++ Assets/Scripts/Rogue/TeleportPlayer.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TeleportPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     private TeleportManager teleportManager;
+    public Tilemap[] symbolTilemaps;
+    public bool[] symbolList = new bool[4];
+
+    bool teleporting = false;
+
     void Start()
     {
         teleportManager = FindObjectOfType<TeleportManager>();
@@ -14,15 +20,28 @@ public class TeleportPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (teleporting)
+        {
+            
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            teleporting = true;
+
             collision.transform.position = teleportManager.getBossRoom().position;
             teleportManager.teleported(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            teleporting = false;
         }
     }
 }
