8421522a5462c5a499c80623fb38974ca67e74a7
jiulen <wongjiulen.coding@gmail.com>
2023-08-07 15:20:58 +0800

    chest animation done

--- Assets/ChestManager.cs
+++ Assets/ChestManager.cs
@@ -11,11 +11,15 @@ public class ChestManager : MonoBehaviour
     [SerializeField] private GameObject[] possibleUpgrades;
     SpriteRenderer spriteRenderer;
 
+    Animator chestAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         hp = startHp;
+
+        chestAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,7 +34,7 @@ public class ChestManager : MonoBehaviour
 
     public void Damage()
     {
-        if (!isInvincible)
+        if (!isInvincible && hp > 0)
         {
             hp--;
             if (hp <= 0)
@@ -40,15 +44,25 @@ public class ChestManager : MonoBehaviour
                 GameObject upgradeToDrop = possibleUpgrades[dropIndex];
 
                 //Drop stuff
-                Instantiate(upgradeToDrop, transform.position, transform.rotation);
+                StartCoroutine(SpawnDrop(upgradeToDrop));
 
-                //Destroy itself
-                Destroy(gameObject);
+                chestAnimator.Play("ChestOpen", spriteRenderer.sortingLayerID, 0);
+                
                 return;
             }
+            else
+            {
+                chestAnimator.Play("ChestHit", spriteRenderer.sortingLayerID, 0);
+            }
 
-            invincibleTimer = 0.01f;
+            invincibleTimer = 0.3f;
             isInvincible = true;
         }
+    }
+
+    IEnumerator SpawnDrop(GameObject upgradeDropped)
+    {
+        yield return new WaitForSeconds(0.375f);
+        Instantiate(upgradeDropped, transform.position, transform.rotation);
     }
 }
